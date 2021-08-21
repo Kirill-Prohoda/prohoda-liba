8aa61ed96ad94c76b77f2183df29d2e090a4e09f
kirill <prohodakirill@gmail.com>
2021-08-21 14:26:26 +0400

    del

--- src/components/InputContainer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-export let InputContainer = (props) =>{
-    let {placeholder} = props
-    return (
-        <div>
-            <input placeholder={placeholder} />
-        </div>
-    )
-}
